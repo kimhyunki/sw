5cdb9b323e265500958d596dcb8d0bd78f1a0a85
김현기 <khkraining@falinux.com>
2019-01-08 11:08:23 +0900

    obj file del

--- linux/device-driver/001_driver-simple/.simple.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/khk/work/sw/linux/device-driver/001_driver-simple/simple.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/khk/work/sw/linux/device-driver/001_driver-simple/simple.ko /home/khk/work/sw/linux/device-driver/001_driver-simple/simple.o /home/khk/work/sw/linux/device-driver/001_driver-simple/simple.mod.o
--- linux/device-driver/001_driver-simple/.tmp_versions/simple.mod
+++
@@ -1,2 +0,0 @@
-/home/khk/work/sw/linux/device-driver/001_driver-simple/simple.ko
-/home/khk/work/sw/linux/device-driver/001_driver-simple/simple.o
