667b284fc72f6e0b6a936b1ed6cac6ccebb6a8a4
김현기 <khkraining@falinux.com>
2019-05-10 18:27:44 +0900

    git submodule test

--- git/submodules/run2.sh
+++ git/submodules/run2.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 
-#rm test0 -rf
-#git clone --recursive ssh://git@git.falinux.com:10022/khkraining/test0.git
-#cd test0
-#
-#git checkout -b stable
-#git submodule update --remote --merge
-#
-#cd submodule_dir
-#touch 2
-#git add . 
-#git commit -am 'touch 2'
-#cd ../
-#
-#git submodule update --remote --rebase
+rm test0 -rf
+git clone --recursive ssh://git@git.falinux.com:10022/khkraining/test0.git
+cd test0
+
+git checkout -b stable
+git submodule update --remote --merge
+
+cd submodule_dir
+touch 2
+git add . 
+git commit -am 'touch 2'
+cd ../
+
+git submodule update --remote --rebase
+
+exit
 
 git add .
 git commit . -m "test 2"
