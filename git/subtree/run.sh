d53a2d6e72b5f60d43f94f012156a57735974ac5
김현기 <khkraining@falinux.com>
2019-05-10 11:31:29 +0900

    added test module

--- git/subtree/run.sh
+++ git/subtree/run.sh
@@ -3,6 +3,29 @@
 # git clone ssh://git@git.falinux.com:10022/khkraining/test.git
 # https://help.github.com/en/articles/about-git-subtree-merges
 
+
+if [ ! -d test ]; then
+git clone ssh://git@git.falinux.com:10022/khkraining/test.git
+fi
+
+if [ ! -d test1 ]; then
+git clone ssh://git@git.falinux.com:10022/khkraining/test1.git
+fi
+
+# test에 test1에 추가한다.
+cd test
+git remote add test1 ssh://git@git.falinux.com:10022/khkraining/test1.git
+git fetch test1 --no-tags
+git checkout -b test_branch test1/master
+
+# test1 프로젝트를 test의 하위디렉토리로 만들수 있다.
+#git read-tree --prefix=test1 -u test_branch
+#git checkout master 
+#git merge --squash -s recursive -Xsubtree=test1 test_branch
+cd ../
+
+exit
+
 mkdir test
 cd test
 git init
