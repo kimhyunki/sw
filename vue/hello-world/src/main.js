33fe9d5ca33625c6ec81b1e0335141819da269df
kimhyunki <khkraining@gmail.com>
2020-02-20 14:49:01 +0900

    Automated commit on 2020-02-20 14:49:01 KST

--- vue/hello-world/src/main.js
+++ vue/hello-world/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import vue from 'vue'
-import App from './App.vue;
+import App from './App.vue'
 
 new vue ({
   el: '#app',
